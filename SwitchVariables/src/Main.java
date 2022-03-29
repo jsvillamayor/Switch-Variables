4e0671debbca4832d2744bd8fa4547e348f62432
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 12:24:29 -0700

    Add files via upload

---
+++ SwitchVariables/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x = 1;
+		int y = 2;
+		int temp;
+		
+		temp = x;
+		x=y;
+		y=temp;
+		
+		System.out.println("x: "+x);
+		System.out.println("y: "+y);
+
+	}
+
+}
